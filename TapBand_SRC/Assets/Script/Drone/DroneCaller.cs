5bf79a4a6e7df31e0745573a24d3791376f4c9dc
Milan Harsfalvai <milan.harsfalvai@gmail.com>
2016-05-12 19:51:31 +0200

    Drone First Scripts

---
+++ TapBand_SRC/Assets/Script/Drone/DroneCaller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneCaller : MonoBehaviour {
+
+    public GameObject Drone;
+    public int DroneMaxInterval = 10;
+    public int DroneMinDelay = 10;
+    public int DroneMaxDelay = 30;
+    int deltaTime;
+    //int deltaTime = Random.Range(DroneMinDelay, DroneMaxDelay);
+
+    // Use this for initialization
+    void Start () {
+        print("Elindultam");
+        deltaTime = Random.Range(3, 7);
+        StartCoroutine(waitSomeSeconds());
+    }
+	
+
+    IEnumerator waitSomeSeconds()
+    {
+        yield return new WaitForSeconds(deltaTime);
+        print("Folytatom");
+        yield return new WaitForSeconds(3);
+        yield return 0;
+        //Application.Quit();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
